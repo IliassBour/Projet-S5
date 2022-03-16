947a63c2499230a55371dbc1fd56524a48cad8c6
Iliass b <iliass.bourabaa@usherbrooke.ca>
2022-03-16 13:39:46 -0400

    Initial calcul

--- calculTrajet.py
+++ calculTrajet.py
@@ -1,6 +1,17 @@
 import numpy as np
 
 def calculTrajet():
+    a_max=1.06
+    d1 = 0.3
+    d2 = 0.1
+    s= np.sqrt((d1-d2)/a_max)
+    v_max=(d1-d2)/s
+
+    dX = []
+    dY = []
+    t = []
+
+
     return 0
 
 if __name__ == '__main__':
