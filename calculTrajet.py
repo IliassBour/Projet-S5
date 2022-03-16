457af42ec941100858214052f5512952352a6c1b
Iliass b <iliass.bourabaa@usherbrooke.ca>
2022-03-16 13:50:32 -0400

    Constante

--- calculTrajet.py
+++ calculTrajet.py
@@ -1,16 +1,30 @@
 import numpy as np
 
-def calculTrajet():
-    a_max=1.06
+class Constant(object):
+    rayon= 0.14
+    angle_max=8.21
+    pourcentage_angle=0.75
+
+
+def vitesseMax(a_max):
     d1 = 0.3
     d2 = 0.1
     s= np.sqrt((d1-d2)/a_max)
-    v_max=(d1-d2)/s
+    return (d1-d2)/s
 
-    dX = []
-    dY = []
+def rayonRoue():
+    return 0
+
+def calculTrajet():
+    #Avance
+    d = []
     t = []
 
+    #Arret
+
+    #Tournant droit
+
+    #Tournant gauche
 
     return 0
 
