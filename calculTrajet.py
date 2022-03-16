90d620c817498ad35aec3f42fcc8fd3a37cf70b2
Iliass b <iliass.bourabaa@usherbrooke.ca>
2022-03-16 14:20:55 -0400

    Angle de la roue

--- calculTrajet.py
+++ calculTrajet.py
@@ -1,10 +1,12 @@
 import numpy as np
 
 class Constant(object):
-    rayon= 0.14
-    angle_max=8.21
+    rayon= 1 #métre
+    angle_max=8.21 #degrée
     pourcentage_angle=0.75
 
+    largeur_roue = 0.134 #métre (track_withd)
+    longueur_roue = 0.25 #métre (wheelbase)
 
 def vitesseMax(a_max):
     d1 = 0.3
@@ -12,8 +14,11 @@ def vitesseMax(a_max):
     s= np.sqrt((d1-d2)/a_max)
     return (d1-d2)/s
 
-def rayonRoue():
-    return 0
+def rayonRoue(a_max):
+    angle_int = np.arctan(Constant.longueur_roue/(Constant.rayon-Constant.largeur_roue/2))
+    angle_ext = np.arctan(Constant.longueur_roue/(Constant.rayon+Constant.largeur_roue/2))
+    moy_angle = (angle_int+angle_ext)/2
+    return np.rad2deg(moy_angle)
 
 def calculTrajet():
     #Avance
